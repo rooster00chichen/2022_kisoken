143230d2f32c2ff28463af6b2d1535ab99267035
rooster00chichen <niwatoriyuya08@gmail.com>
2022-07-04 10:24:09 +0900

    add def do_fft()

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import serial
 # pyqtgraphとpyqt6を用インストール、numpyも念の為
 
 ser = serial.Serial("COM3", 9600, 8, timeout=50*10**-3)
-toSer=True
+toSer = True
 n_samples = 5001
 i = 0
 num_list = np.zeros(n_samples)
@@ -15,33 +15,41 @@ win = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
 win.resize(1000, 600)
 win.setWindowTitle('show_wave_form')
 pg.setConfigOptions(antialias=True)
-p6 = win.addPlot(title="Voltage plot")
-curve = p6.plot(pen='y')
-p6.setLimits(yMin=0.7,yMax=1.2,xMin=0,xMax=5000)
-p6.setYRange(0.7,1.2)
-p6.setMouseEnabled(x=True, y=False)
+
+p1 = win.addPlot(title="Voltage plot")
+curve_1 = p1.plot(pen='y')
+p1.setLimits(yMin=0.7, yMax=1.2, xMin=0, xMax=5000)
+p1.setMouseEnabled(x=True, y=False)
+
+p2 = win.addPlot(title="frequency plot")
+curve_2 = p2.plot(pen='xy')
+#p6.setLimits(yMin=0.7, yMax=1.2, xMin=0, xMax=5000)
+p2.setMouseEnabled(x=False, y=False)
 
 
 def update():
-    global curve, p6, i,toSer,ser
+    global curve_1, i, toSer, ser, curve_2
     if not toSer:
         try:
             # シリアル
             ser = serial.Serial("COM3", 9600, 8, timeout=50*10**-3)
-            toSer=True
+            toSer = True
         except:
             pass
     try:
         msg = ser.readline().decode()
     except:
         msg = "0"
-        toSer=False
+        toSer = False
 
-    if msg!='':
+    if msg != '':
         i = i % n_samples
         num_list[i] = msg
         pos = i + 1 if i < n_samples else 0
-        curve.setData(np.r_[num_list[pos:n_samples], num_list[0:pos]])
+        wave_date1 = np.r_[num_list[pos:n_samples], num_list[0:pos]]
+        frequency, wave_amp = do_fft(wave_date1)
+        curve_1.setData(wave_date1)
+        curve_2.setData(x=frequency, y=wave_amp)
         i += 1
 
 
@@ -56,6 +64,18 @@ def main():
         QtGui.QApplication.instance().exec_()
     if toSer:
         ser.close()
+
+
+def do_fft(wave_date):
+    sampling_cycle = 10*10**-3
+    N = len(wave_date)
+    fft_wave = np.fft.fft(wave_date)
+    abs_fft_wave = np.abs(fft_wave)
+    abs_fft_amp = abs_fft_amp / N * 2   # 交流成分
+    abs_fft_amp[0] = abs_fft_amp[0] / 2       # 直流成分
+    frequency = np.linspace(0, 1.0/sampling_cycle, N)
+
+    return frequency, fft_wave
 
 
 if __name__ == "__main__":
--- udp_recv.py
+++ udp_recv.py
@@ -15,31 +15,40 @@ s.bind((IPADDR, PORT))
 
 n_samples = 5001
 i = 0
-num_list = np.ones(n_samples)
+num_list = np.zeros(n_samples)
 app = pg.mkQApp("Plotting Example")
 win = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
 win.resize(1000, 600)
 win.setWindowTitle('pyqtgraph example: Plotting')
 pg.setConfigOptions(antialias=True)
-p6 = win.addPlot(title="Updating plot")
-curve = p6.plot(pen='y')
-p6.setMouseEnabled(x=False, y=False)
-p6.setYRange(0.6, 1.5)
+
+p1 = win.addPlot(title="Voltage plot")
+curve_1 = p1.plot(pen='y')
+p1.setLimits(yMin=0.7, yMax=1.2, xMin=0, xMax=5000)
+p1.setMouseEnabled(x=True, y=False)
+
+p2 = win.addPlot(title="frequency plot")
+curve_2 = p2.plot(pen='r')
+p2.setMouseEnabled(x=True, y=False)
 
 
 def update():
-    global curve, p6, i
+    global curve_1, i, toSer, ser, curve_2
 
     try:
         msg = s.recv(1024).decode()
     except socket.timeout:
-        msg = '1'
+        msg = '0'
 
-    i = i % n_samples
-    num_list[i] = msg
-    pos = i + 1 if i < n_samples else 0
-    curve.setData(np.r_[num_list[pos:n_samples], num_list[0:pos]])
-    i += 1
+    if msg != '':
+        i = i % n_samples
+        num_list[i] = msg
+        pos = i + 1 if i < n_samples else 0
+        wave_date1 = np.r_[num_list[pos:n_samples], num_list[0:pos]]
+        frequency, wave_amp = do_fft(wave_date1)
+        curve_1.setData(wave_date1)
+        curve_2.setData(frequency, wave_amp)
+        i += 1
 
 
 timer = QtCore.QTimer()
@@ -49,6 +58,18 @@ timer.start(10)
 
 def main():
     pg.exec()
+
+
+def do_fft(wave_date):
+    sampling_cycle = 10*10**-3
+    N = len(wave_date)
+    fft_wave = np.fft.fft(wave_date)
+    abs_fft_wave = np.abs(fft_wave)
+    abs_fft_wave = abs_fft_wave / N * 2   # 交流成分
+    abs_fft_wave[0] = abs_fft_wave[0] / 2       # 直流成分
+    frequency = np.linspace(0, 1.0/sampling_cycle, N)
+
+    return frequency, abs_fft_wave
 
 
 if __name__ == "__main__":
